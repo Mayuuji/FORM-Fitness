a9eb59160d802bba85a4ebdecbf9ecb37c664313
haylee4 <marks0444@csusm.edu>
2025-03-17 12:01:07 -0700

    Implemented pose estimation using TensorFlow Lite in Camera2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    androidResources {
+        noCompress += "tflite"
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -49,6 +52,23 @@ dependencies {
     //implementation("androidx.camera:camera-extensions:1.3.0") // Extensions API
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.10.1")
+
+    implementation("org.tensorflow:tensorflow-lite:0.4.2")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.2")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.2")
+
+    // Ensure CameraX dependencies are included
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
+
+    //implementation ("org.tensorflow:tensorflow-lite:2.12.0") {
+      //  exclude(group = "org.tensorflow", module = "tensorflow-lite-task-vision")
+    //}
+    //implementation ("org.tensorflow:tensorflow-lite-task-vision:2.12.0")
+    //implementation("org.tensorflow:tensorflow-lite-support:2.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
